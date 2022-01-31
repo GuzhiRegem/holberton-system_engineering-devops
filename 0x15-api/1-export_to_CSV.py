2c348945f881b600eb1ec891ee4eef2f5cd6a338
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-31 18:21:49 -0300

    commit

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,16 +9,16 @@ import json
 import csv
 
 if __name__ == "__main__":
-	num = sys.argv[1]
-	request_raw = requests.get(
-		'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
-			num))
-	user_raw = requests.get(
-		'https://jsonplaceholder.typicode.com/users/{}'.format(
-			num))
-	username = json.loads(user_raw.text).get("name")
-	request_list = json.loads(request_raw.text)
-	with open('{}.csv'.format(num), 'w', newline='') as csvfile:
-		writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-		for req in request_list:
-			writer.writerow([str(num), username, req.get("completed"), req.get("title")])
+    num = sys.argv[1]
+    request_raw = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+            num))
+    user_raw = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(
+            num))
+    username = json.loads(user_raw.text).get("name")
+    request_list = json.loads(request_raw.text)
+    with open('{}.csv'.format(num), 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for req in request_list:
+            writer.writerow([str(num), username, req.get("completed"), req.get("title")])
