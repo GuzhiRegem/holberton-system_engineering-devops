c12b4e6d1855251bcb4110c5303b57f91b7e7927
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-31 19:16:36 -0300

    commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,14 +15,16 @@ if __name__ == "__main__":
     username = json.loads(user_raw.text).get("name")
     request_list = json.loads(request_raw.text)
     finished = []
+    count = 0
     for a in request_list:
         if str(a.get("userId")) == sys.argv[1]:
             if a.get("completed"):
                 finished.append(a.get("title"))
+            count += 1
     print("Employee {} is done with tasks({}/{}):".format(
         username,
         len(finished),
-        len(request_list)
+        count
     ))
     for a in finished:
-        print("\t{}".format(a))
+        print("\t {}".format(a))
