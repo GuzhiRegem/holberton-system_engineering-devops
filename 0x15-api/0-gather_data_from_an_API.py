c75b0c05d4301525bad7befda1b1cd26d626cb71
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-31 18:50:49 -0300

    commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,8 +8,7 @@ import sys
 
 if __name__ == "__main__":
     request_raw = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
-            sys.argv[1]))
+        'https://jsonplaceholder.typicode.com/todos')
     user_raw = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}'.format(
             sys.argv[1]))
@@ -17,8 +16,9 @@ if __name__ == "__main__":
     request_list = json.loads(request_raw.text)
     finished = []
     for a in request_list:
-        if a["completed"]:
-            finished.append(a.get("title"))
+        if a.get("userId") == sys.argv[1]:
+            if a.get("completed"):
+                finished.append(a.get("title"))
     print("Employee {} is done with tasks({}/{}):".format(
         username,
         len(finished),
