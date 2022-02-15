62d6c798cb61729328cfe59388c94d275304e3ae
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-02-15 13:43:58 -0300

    commit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,7 +2,38 @@
 """
     module
 """
+import json
+import requests
+
+from pygments import highlight
+from pygments.lexers import JsonLexer
+from pygments.formatters import TerminalFormatter
 
 
-def number_of_subscribers(subreddit):
+def top_ten(subreddit):
     """ function """
+    url = 'https://oauth.reddit.com/r/{}/hot?limit=10'.format(subreddit)
+    CLIENT_ID = "HS53oPaWudvShOpWbllhWw"
+    SECRET_ID = "WvKok6fZNsaeE4jfTzGFs-PkvOs1BA"
+
+    auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_ID)
+    token = requests.post(
+        'https://www.reddit.com/api/v1/access_token',
+        auth=auth,
+        data={'grant_type': 'client_credentials'},
+        headers={'user-Agent': 'hbtn'}
+    ).json()['access_token']
+    response = requests.get(
+        url,
+        headers={
+            'user-Agent': 'hbtn',
+            'Authorization': 'bearer {}'.format(token)
+            }
+    )
+
+    data = response.json().get("data")
+    if data is None:
+        print(None)
+    else:
+        for post in data.get("children"):
+            print(post.get("data").get("title"))
