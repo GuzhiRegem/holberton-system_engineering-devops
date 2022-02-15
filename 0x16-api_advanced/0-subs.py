fccd5edbb1a3ae6ede449d9c6b6a83cce60b8e58
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-02-15 12:52:10 -0300

    commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,8 +7,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ function """
+    url = 'https://oauth.reddit.com/r/{}/about'.format(subreddit)
     CLIENT_ID = "HS53oPaWudvShOpWbllhWw"
     SECRET_ID = "WvKok6fZNsaeE4jfTzGFs-PkvOs1BA"
+
     auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_ID)
     token = requests.post(
         'https://www.reddit.com/api/v1/access_token',
@@ -17,12 +19,13 @@ def number_of_subscribers(subreddit):
         headers={'user-Agent': 'hbtn'}
     ).json()['access_token']
     response = requests.get(
-        'https://oauth.reddit.com/r/{}/about'.format(subreddit),
+        url,
         headers={
             'user-Agent': 'hbtn',
             'Authorization': 'bearer {}'.format(token)
             }
     )
+
     try:
         out = response.json()
         return out["data"]["subscribers"]
